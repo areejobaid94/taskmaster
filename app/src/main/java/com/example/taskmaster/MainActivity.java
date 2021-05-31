fc1098d25dcdef953b0c28dfbd4d8c8db58338c4
areejobaid94 <areej.obaid@yahoo.com>
2021-05-31 22:50:27 +0300

    onpro

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -15,67 +15,43 @@ import android.widget.TextView;
 
 import com.amplifyframework.AmplifyException;
 import com.amplifyframework.core.Amplify;
-import com.amplifyframework.datastore.AWSDataStorePlugin;
-import com.amplifyframework.datastore.generated.model.Task;
 import com.example.taskmaster.Databases.AppDataBase;
+import com.example.taskmaster.Models.Task;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private TaskAdpater taskAdpater;
     @Override
     protected void onStart()
     {
         super.onStart();
-        try {
-            Amplify.addPlugin(new AWSDataStorePlugin());
-            Amplify.configure(getApplicationContext());
-            this.handleRViewShow();
-        }catch (AmplifyException ex){
-            System.out.println(ex);
-        }
-    }
-    @Override
-    public void onResume(){
-        super.onResume();
-        System.out.println("here");
-        this.handleRViewShow();
-    }
-
-    private void handleRViewShow(){
-        List<com.example.taskmaster.Models.Task> tasksToView = new ArrayList<>();
-        try {
-            Amplify.DataStore.query(Task.class,
-                    tasks -> {
-                        while (tasks.hasNext()) {
-                            Task task = tasks.next();
-                            tasksToView.add(new com.example.taskmaster.Models.Task(task.getTitle(),task.getBody(),task.getState()));
-                            System.out.println(task);
-                        }
-                    },
-                    failure -> Log.e("Tutorial", "Could not query DataStore", failure)
-            );
-            try{
-                Thread.sleep(1500);
-                RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rec_id_new);
-                TaskAdpater taskAdpater = new TaskAdpater(this,tasksToView);
-                recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-                recyclerView.setAdapter(taskAdpater);
-            }catch(InterruptedException e){
-                e.printStackTrace();
-            }
-
-        } catch (Exception e) {
-            Log.e("Tutorial", "Could not initialize Amplify", e);
-        }
+        List<Task> tasks = AppDataBase.getAppDataBase(getApplicationContext()).taskDao().getAll();
+        taskAdpater = new TaskAdpater(this,tasks);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(taskAdpater);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.onStart();
-        System.out.println("Start After");
         setContentView(R.layout.activity_main);
+//        setupRecyclerView(R.layout.task);
+        try {
+            Amplify.configure(getApplicationContext());
+
+            Log.i("MyAmplifyApp", "Initialized Amplify");
+        } catch (AmplifyException e) {
+            Log.e("MyAmplifyApp", "Could not initialize Amplify", e);
+        }
         android.text.format.DateFormat.getDateFormat(getApplicationContext());
+        recyclerView = (RecyclerView) findViewById(R.id.rec_id_new);
+//        tasks.add(new Task("Task 1", "The first task body",2));
+//        tasks.add(new Task("Task 2", "The 2nd task body",1));
+//        tasks.add(new Task("Task 3", "The 3rd task body",0));
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         TextView welcome =  findViewById(R.id.usernameTasks);
         if(preferences.getString("username","User") != ""){
@@ -113,5 +89,42 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(k);
             }
         });
+
+//
+//        Button task1 =  findViewById(R.id.task1);
+//        task1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view)
+//            {
+//                Intent k = new Intent(MainActivity.this, TaskDetailActivity.class);
+//                k.putExtra("title","Task 1");
+//                k.putExtra("description","Description for Task 1");
+//                startActivity(k);
+//            }
+//        });
+//        Button task2 =  findViewById(R.id.task2);
+//        task2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view)
+//            {
+//                Intent k = new Intent(MainActivity.this, TaskDetailActivity.class);
+//                k.putExtra("title","Task 2");
+//                k.putExtra("description","Description for Task 2");
+//                startActivity(k);
+//            }
+//        });
+//
+//        Button task3 =  findViewById(R.id.task3);
+//        task3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view)
+//            {
+//                Intent k = new Intent(MainActivity.this, TaskDetailActivity.class);
+//                k.putExtra("title","Task 3");
+//                k.putExtra("description","Description for Task 3");
+//
+//                startActivity(k);
+//            }
+//        });
     }
 }
---
+++ app/src/main/java/com/example/taskmaster/Models/State.java
@@ -0,0 +1,8 @@
+package com.example.taskmaster.Models;
+
+public enum State {
+    NEW,
+    ASSIGNED,
+    InProgress,
+    Complete
+}
